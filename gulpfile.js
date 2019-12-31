ea82cf1ece4a2be9aad6286ed480cccf977dee4b
yonicb <yonicb20@gmail.com>
2019-12-30 19:31:30 -0500

    feat: 🎸 Created gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,53 @@
+var gulp = require("gulp");
+var ts = require("gulp-typescript");
+var tsProject = ts.createProject("tsconfig.json")
+
+var uglify = require("gulp-uglify");
+var sourcemaps = require("gulp-sourcemaps");
+var buffer = require("vinyl-buffer");
+var del = require('del');
+
+function clean(cb) {
+    del(["dist/*.map"])
+    cb()
+}
+
+function dev() {
+    return tsProject.src()
+        .pipe(tsProject())
+        .js.pipe(gulp.dest("dist"))
+        .pipe(buffer())
+        .pipe(sourcemaps.init({ loadMaps: true }))
+        .pipe(uglify())
+        .pipe(sourcemaps.write('./'))
+}
+function build() {
+    return tsProject.src()
+        .pipe(tsProject())
+        .js.pipe(gulp.dest("dist"))
+        .pipe(buffer())
+        .pipe(sourcemaps.init({ loadMaps: true }))
+        .pipe(uglify())
+        .pipe(sourcemaps.write('./'))
+    // .pipe(gulp.dest('dist'));
+}
+function message(cb, msg = `
+    ========================
+       Server dev started
+    ========================
+`) {
+    return new Promise(function (resolve, reject) {
+        console.log(msg);
+        resolve()
+    })
+}
+gulp.task("clean", clean)
+gulp.task("default", function () {
+    return gulp.watch('./src/index.ts', gulp.series(message, dev))
+})
+
+gulp.task("prod", gulp.series(build, clean, () => message(null, `
+    ===============================
+    Build Successfully completion !
+    ==============================
+`)))
