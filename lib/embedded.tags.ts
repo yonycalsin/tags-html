725219c48ad506a2b8e147042ecfd72bc50b1939
yonicb <yonicb20@gmail.com>
2019-12-30 23:01:22 -0500

    feat: 🎸 Created Embedded Content Tags

---
+++ lib/embedded.tags.ts
@@ -0,0 +1,19 @@
+/**
+ * Embedded Content
+ */
+export default [
+    'img',
+    'iframe',
+    'embed',
+    'object',
+    'param',
+    'video',
+    'audio',
+    'source',
+    'track',
+    'canvas',
+    'map',
+    'area',
+    'svg',
+    'math',
+];
