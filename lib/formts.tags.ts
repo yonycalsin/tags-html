621fe63bbb5995b5b8b0b3658bbaa46dbe7e52f5
yonicb <yonicb20@gmail.com>
2019-12-30 23:01:50 -0500

    feat: 🎸 Created forms tags

---
+++ lib/formts.tags.ts
@@ -0,0 +1,20 @@
+/**
+ * Forms
+ */
+export default [
+    'form',
+    'fieldset',
+    'legend',
+    'label',
+    'input',
+    'button',
+    'select',
+    'datalist',
+    'optgroup',
+    'option',
+    'textarea',
+    'keygen',
+    'output',
+    'progress',
+    'meter',
+];
