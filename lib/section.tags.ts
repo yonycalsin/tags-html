537e6b9e31258389017b06b0e88e8d11e55cf1d2
yonicb <yonicb20@gmail.com>
2019-12-30 23:03:52 -0500

    feat: 🎸 Created Sections Tags

---
+++ lib/section.tags.ts
@@ -0,0 +1,20 @@
+/**
+ * Sections
+ */
+export default [
+    'body',
+    'section',
+    'nav',
+    'article',
+    'aside',
+    'h1',
+    'h2',
+    'h3',
+    'h4',
+    'h5',
+    'h6',
+    'header',
+    'footer',
+    'address',
+    'main',
+];
