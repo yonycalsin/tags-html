9089a9e7ffd28edd594a6c0ad2125ca6abfa342f
yonicb <yonicb20@gmail.com>
2019-12-30 23:02:13 -0500

    feat: 🎸 Created Content Grouping Tags

---
+++ lib/group-content.tags.ts
@@ -0,0 +1,18 @@
+/**
+ * Content Grouping
+ */
+export default [
+    'p',
+    'hr',
+    'pre',
+    'blockquote',
+    'ol',
+    'ul',
+    'li',
+    'dl',
+    'dt',
+    'dd',
+    'figure',
+    'figcaption',
+    'div',
+];
