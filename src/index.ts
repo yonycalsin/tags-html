9bda2fb9de4560914bd2b2f035a13e43aa25d489
yonicb <yonicb20@gmail.com>
2019-12-30 23:00:24 -0500

    feat: 🎸 Created main file

---
+++ src/index.ts
@@ -0,0 +1,45 @@
+/*!
+ * Tags-Html v1.0.0 (https://github.com/yonicb/tags-html/)
+ * Copyright 2019 The Tags-Html Author
+ * Copyright 2019 Yoni Calsin.
+ * Licensed under MIT (https://github.com/yonicb/tags-html/blob/master/LICENSE)
+ */
+import rootTags from '../lib/root.tags';
+import metadataTags from '../lib/metadata.tags';
+import scriptingTags from '../lib/scripting.tags';
+import sectionsTags from '../lib/section.tags';
+import groupContentTags from '../lib/group-content.tags';
+import textSemanticsTags from '../lib/text-semantics.tags';
+import editionsTags from '../lib/edition.tags';
+import embeddedTags from '../lib/embedded.tags';
+import tabularTags from '../lib/tabular.tags';
+import formsTags from '../lib/formts.tags';
+import interactiveTags from '../lib/interactive.tags';
+
+// All tags HTML5
+const allTags = [
+   ...rootTags,
+   ...metadataTags,
+   ...scriptingTags,
+   ...sectionsTags,
+   ...groupContentTags,
+   ...textSemanticsTags,
+   ...editionsTags,
+   ...embeddedTags,
+   ...tabularTags,
+   ...formsTags,
+   ...interactiveTags,
+];
+
+module.exports = allTags;
+module.exports.rootTags = rootTags;
+module.exports.metadataTags = metadataTags;
+module.exports.scriptingTags = scriptingTags;
+module.exports.sectionsTags = sectionsTags;
+module.exports.groupContentTags = groupContentTags;
+module.exports.textSemanticsTags = textSemanticsTags;
+module.exports.editionsTags = editionsTags;
+module.exports.embeddedTags = embeddedTags;
+module.exports.tabularTags = tabularTags;
+module.exports.formsTags = formsTags;
+module.exports.interactiveTags = interactiveTags;
